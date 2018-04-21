faf784d431229ecca51035b3cb17d23c57a04601
P4tCr4ft <patrickcarnivore@gmail.com>
2018-04-21 17:22:46 +0930

    Initial comments

---
+++ Analog_clock_equidistant_hands/equidistant_hands.py
@@ -0,0 +1,11 @@
+'''
+
+Assumptions:
+
+This script solves this problem using 360 decimal degrees in a circle.
+Every second, each hand moves a discrete distance.
+Every second, the second hand moves 6.0 degrees.
+Every second, the minute hand moves 0.1 decimal degrees.
+Every second, the hour hand moves (0.1/12) decimal degrees.
+In one complete cycle of the analog clock the second hand moves 43,200 times.
+'''
