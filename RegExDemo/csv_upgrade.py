a40ca97a07465f1670fe9575600699fd5394383a
STEVESPRO3\Steve <patrickcarnivore@gmail.com>
2018-08-08 20:40:58 +0930

    8th August 2018. Making regex progress

--- RegExDemo/csv_upgrade.py
+++ RegExDemo/csv_upgrade.py
@@ -2,13 +2,39 @@ import csv
 import re
 
 def parse_csv():
+    # parse function
+    pass
 
 
-if __name__ == "main":
+if __name__ == '__main__':
+
+    row = ''
+    text = 'Guido will be out of the office from 12/15/2012 - 1/3/2013.'
 
     with open('CSVData.csv', 'r') as f:
         for r in csv.reader(f):
-            print(r)
+            # print(r)
+            for item in r:
+                row += item
+            # print(row)
+            # p = re.compile('\d+')# works for first date number
+            # p = re.compile(r'[DRA]')
+            # p = re.compile('a[bcd]*b')# works as per python docs example
+            # p = re.compile(r'\d\d\d')# No doesn't work
+            p = re.compile('\d+/\d+/\d+')# Yes this works with finditer
+
+            # m = p.match(text)# I think match starts from the start, may not be best option
+            for m in p.finditer(text):# Yes this works
+                print(m.group())
+
+
+            # m = p.match(row)
+            # m = p.match('abcbd')# works as per python docs example
+            if m:
+                print(m.group())
+            else:
+                print('m is empty, nothing found')
+            row = ''
 
     f.close()
 
