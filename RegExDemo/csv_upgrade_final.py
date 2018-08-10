9df40e723d241a8490fc4042662f1925a5550011
STEVESPRO3\Steve <patrickcarnivore@gmail.com>
2018-08-10 16:10:13 +0930

    10th August 2018. adding balance calcs

--- RegExDemo/csv_upgrade_final.py
+++ RegExDemo/csv_upgrade_final.py
@@ -3,16 +3,19 @@ import re
 
 if __name__ == '__main__':
 
-    regex = '[-+]+\d+\.*\d*'
+    regex_dollar_val = '[-+]+\d+\.*\d*'
+
+    starting_balance = input('Enter starting balance: $')
 
     row = ''
 
     with open('CSVData.csv', 'r') as f:
         for r in csv.reader(f):
+            print('r out of csv is {}'.format(r))
             for item in r:
                 row += item
 
-            p = re.compile(regex)
+            p = re.compile(regex_dollar_val)
             # p = re.compile('[-+]+\d+\.*\d*')
 
             m = p.search(row)
@@ -22,4 +25,21 @@ if __name__ == '__main__':
                 print('m is empty, no results')
 
             row = ''
+
+            # r += m.group()
+            # r.append(m.group())
+            dollar_val = m.group()
+
+            if dollar_val[0] == '-':
+                dollar_val = float(dollar_val[1:]) * -1
+            else:
+                dollar_val = float(dollar_val)
+
+            balance = float(starting_balance) + dollar_val
+
+            r.append(balance)
+
+
+            print('new r is {}'.format(r))
+
     f.close()
