727a86a7013e6f2e20da54e269cbce6e74617b1b
STEVESPRO3\Steve <patrickcarnivore@gmail.com>
2018-10-22 09:06:17 +1030

    22 Oct 2018. Adding Python Beyond the Basics exercises exception

--- Udemy_PythonBeyondtheBasics_Sec3/assignments_steve.py
+++ Udemy_PythonBeyondtheBasics_Sec3/assignments_steve.py
@@ -1,6 +1,12 @@
 class MaxSizeList(object):
 
     def __init__(self, list_size):
+        try:
+            self.list_size = int(list_size)
+        except TypeError as e:
+            print('Size argument is wrong type'.format(e))
+        except Exception as e:
+            print('Some other argument error'.format(e.__str__()))
         self.list_size = list_size
         self.list = []
 
