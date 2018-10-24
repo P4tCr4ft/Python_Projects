a668f868faa36ad4359601674b53fff012f3908a
P4tCr4ft <patrickcarnivore@gmail.com>
2018-10-24 17:01:25 +1030

    24 Oct 2018. Section 4 exercises - inheritance and calling various constructors with super

--- Udemy_PythonBeyondtheBasics_Sec4/calling_parent_constructor_super.py
+++ Udemy_PythonBeyondtheBasics_Sec4/calling_parent_constructor_super.py
@@ -4,11 +4,13 @@ import random
 
 class Animal(object):
     def __init__(self, name):
+        print('Animal constructor')
         self.name = name
 
 
 class Dog(Animal):
     def __init__(self, name):
+        print('Dog constructor')
         # Python 2 syntax below, needs class which you're calling parent of, and the Dog object, self
         # super(Dog, self).__init__(name)
 
@@ -18,3 +20,19 @@ class Dog(Animal):
 
     def fetch(self, thing):
         print('{0} ran and fetched the {1}'.format(self.name, thing))
+
+
+class Puppy(Dog):
+    def __init__(self, name, age):
+        print('Puppy constructor')
+        super(Dog, self).__init__(name)
+        self.age = age
+
+
+# Can specify calling the constructor of the primary child class Dog, which
+# actually skips calling the constructors of those in the hierarchy between Runt and Dog
+class Runt(Puppy):
+    def __init__(self, name, age):
+        print('Runt constructor')
+        super(Dog, self).__init__(name)
+        self.risk_factor = 10
--- Udemy_PythonBeyondtheBasics_Sec4/test.py
+++ Udemy_PythonBeyondtheBasics_Sec4/test.py
@@ -1,15 +1,20 @@
 # coding=utf-8
 
-from calling_parent_constructor_super   import Dog
+from calling_parent_constructor_super   import Dog, Puppy, Runt
 from static_and_class_methods_decorators import InstanceCounter, InstanceCounter2
 
 
 # calling parent class constructor demo
-rover = Dog('Rover')
+# rover = Dog('Rover')
+# rover = Puppy('Rover', 2)
+rover = Runt('Rover', 2)
 
-print('The dogs name is {0} and the breed is {1}'.format(rover.name, rover.breed))
+# print('The dogs name is {0} and the breed is {1}'.format(rover.name, rover.breed))
+# print('The dogs age is {0}'.format(rover.age))
 rover.fetch('shoe')
 print()
+
+
 
 
 # static and class methods, and decorator example
