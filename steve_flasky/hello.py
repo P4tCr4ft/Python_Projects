3136b2146ee4df42eb74640f9edd058422bbc47d
--global <patrickcarnivore@gmail.com>
2020-05-23 20:10:27 +0930

    up to ch4 forms

--- steve_flasky/hello.py
+++ steve_flasky/hello.py
@@ -1,14 +1,31 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
+
+from flask_wtf import FlaskForm
+from wftorms import StringField, SubmitField
+from wftorms.validators import DataRequired
+
+class NameForm(FlaskForm):
+	name = StringField('What is your name?', validators=[DataRequired()])
+	submit = SubmitField('Submit')
 
 app = Flask(__name__)
 
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+	name = None
+	form = NameForm()
+	if form.validate_on_submit():
+		name = form.name.data
+		form.name.data = ''
+    return render_template('index.html', form=form, name=name,
+    	current_time=datetime.utcnow())
 
 
 @app.route('/user/<name>')
