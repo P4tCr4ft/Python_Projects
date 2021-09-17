31f4ecbf3ad5754876b5554e8f2c33558a402013
Steve <SJTriplett@DESKTOP-BKSG66B>
2021-09-17 17:53:27 +0930

    Add static files and complete templates

--- FastAPI_realistic_weather_api/main.py
+++ FastAPI_realistic_weather_api/main.py
@@ -5,15 +5,18 @@
 import fastapi
 import uvicorn
 from starlette.requests import Request
+from starlette.staticfiles import StaticFiles
 from starlette.templating import Jinja2Templates
 
 api = fastapi.FastAPI()
 templates = Jinja2Templates('templates')
+api.mount('/static', StaticFiles(directory='static'), name='static')
 
 
 @api.get('/')
 def index(request: Request):
     # return "Hello Weather App!"
+    # return templates.TemplateResponse('home/index.html', {'request': request, 'other': [1,2,3]})
     return templates.TemplateResponse('home/index.html', {'request': request})
 
 if __name__ == '__main__':
