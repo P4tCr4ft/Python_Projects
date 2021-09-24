ab1bc2d95721ec9be9711317b04d910491a1c498
P4tCr4ft <patrickcarnivore@gmail.com>
2021-09-24 12:15:32 +0930

    FastAPI add favicon and update gitignore

--- FastAPI_realistic_weather_api/main.py
+++ FastAPI_realistic_weather_api/main.py
@@ -19,5 +19,17 @@ def index(request: Request):
     # return templates.TemplateResponse('home/index.html', {'request': request, 'other': [1,2,3]})
     return templates.TemplateResponse('home/index.html', {'request': request})
 
+
+@api.get('/favicon.ico')
+def favicon():
+    return fastapi.responses.RedirectResponse(url='/static/img/test_favicon.ico')
+
+
+@api.get('/api/weather')
+def weather():
+    return 'some report'
+
+
+
 if __name__ == '__main__':
     uvicorn.run(api, port=8000, host='127.0.0.1')
