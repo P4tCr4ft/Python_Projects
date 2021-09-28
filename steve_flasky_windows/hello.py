6013b1bb7d2a8b7bd8c30ed44555fa4549e6c829
P4tCr4ft <patrickcarnivore@gmail.com>
2021-09-29 07:47:38 +0930

    Ch5 add sqlite db and some db model classes

--- steve_flasky_windows/hello.py
+++ steve_flasky_windows/hello.py
@@ -3,13 +3,10 @@ from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
-
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
-
 from flask_sqlalchemy import SQLAlchemy
-
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
@@ -43,30 +40,45 @@ class User(db.Model):
         return '<User %r>' % self.username
 
 
-class NameForm(FlaskForm) :
+class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    # Remember session is different from db.session
     form = NameForm()
     if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('Looks like you have changed your name!')
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            db.session.commit()
+            session['known'] = False
+        else:
+            session['known'] = True
         session['name'] = form.name.data
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'), current_time=datetime.utcnow())
-    # return render_template('index.html', form=form, name=name)
+    return render_template('index.html', form=form, name=session.get('name'),
+                           known=session.get('known', False))
+
+# @app.route('/', methods=['GET', 'POST'])
+# def index():
+#     form = NameForm()
+#     if form.validate_on_submit():
+#         old_name = session.get('name')
+#         if old_name is not None and old_name != form.name.data:
+#             flash('Looks like you have changed your name!')
+#         # Remember session is different from db.session
+#         session['name'] = form.name.data
+#         return redirect(url_for('index'))
+#     return render_template('index.html', form=form, name=session.get('name'), current_time=datetime.utcnow())
 
 
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html', name=name)
-
-
-# return render_template('user.htm', name=name)
+# @app.route('/user/<name>')
+# def user(name):
+#     return render_template('user.html', name=name)
 
 
 @app.errorhandler(404)
