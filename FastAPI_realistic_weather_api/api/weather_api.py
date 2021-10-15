8784bcabc7a6e304f87c02775a630484dbd34796
Steve <SJTriplett@DESKTOP-BKSG66B>
2021-10-15 10:33:03 +1030

    Add first service calling weather API

--- FastAPI_realistic_weather_api/api/weather_api.py
+++ FastAPI_realistic_weather_api/api/weather_api.py
@@ -1,15 +1,15 @@
 from typing import Optional
 
 import fastapi
+from fastapi import Depends
+
+from models.location import Location
+from services import openweather_service
 
 router = fastapi.APIRouter()
 
 
 @router.get('/api/weather/{city}')
-def weather(city: str,
-            country: str,
-            state: Optional[str] = None,
-            # country: str = 'US',
-            units: Optional[str] = 'metric'):
-    # return 'Some report for ' + city
-    return f"{city}, {state}, {country} in {units}"
+def weather(loc: Location = Depends(), units: Optional[str] = 'metric'):
+    report = openweather_service.get_report(loc.city, loc.state, loc.country, units)
+    return report
