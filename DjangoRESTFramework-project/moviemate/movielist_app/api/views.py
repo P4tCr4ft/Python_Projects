a7c14152b05b2954760e79aa1c612dd269216446
P4tCr4ft <patrickcarnivore@gmail.com>
2022-06-17 10:24:50 +0930

    Remove commented out code attempts

--- DjangoRESTFramework-project/moviemate/movielist_app/api/views.py
+++ DjangoRESTFramework-project/moviemate/movielist_app/api/views.py
@@ -5,17 +5,6 @@ from movielist_app.models import Movie
 from movielist_app.api.serializers import MovieSerializer
 
 
-# @api_view()
-# def movie_list(request):
-#     temp_list = []
-#     movies = Movie.objects.all()
-#     for item in list(movies.values()):
-#         movie = Movie.objects.get(pk=item['id'])
-#         serializer = MovieSerializer(movie)
-#         temp_list.append(serializer.data)
-#     return Response({'movies': temp_list})
-
-# I was on the right track above, dealing with individual movie. But it is this simple ...
 @api_view(['GET', 'POST'])
 def movie_list(request):
     
