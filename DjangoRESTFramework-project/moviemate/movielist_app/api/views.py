1671181e8fe9db738c687a6064bf7e6ef1b58271
P4tCr4ft <patrickcarnivore@gmail.com>
2022-06-17 13:22:45 +0930

    Up to Classed based view - APIView class

--- DjangoRESTFramework-project/moviemate/movielist_app/api/views.py
+++ DjangoRESTFramework-project/moviemate/movielist_app/api/views.py
@@ -1,5 +1,5 @@
-from uuid import RESERVED_FUTURE
 from rest_framework.response import Response
+from rest_framework import status
 from rest_framework.decorators import api_view
 from movielist_app.models import Movie
 from movielist_app.api.serializers import MovieSerializer
@@ -21,8 +21,28 @@ def movie_list(request):
         else:
             return Response(serializer.errors)
 
-@api_view()
+@api_view(['GET', 'PUT', 'DELETE'])
 def movie_details(request, pk):
-    movie = Movie.objects.get(pk=pk)
-    serializer = MovieSerializer(movie)
-    return Response(serializer.data)
+    
+    if request.method == 'GET':
+        try:
+            movie = Movie.objects.get(pk=pk)
+        except Movie.DoesNotExist:
+            return Response({'error': 'Movie not found'}, status=status.HTTP_404_NOT_FOUND)
+        serializer = MovieSerializer(movie)
+        return Response(serializer.data)
+
+    if request.method == 'PUT':
+        movie = Movie.objects.get(pk=pk)
+        serializer = MovieSerializer(movie, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+    
+    if request.method == 'DELETE':
+        movie = Movie.objects.get(pk=pk)
+        movie.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
